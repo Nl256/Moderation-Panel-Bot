8b3ffe9d941f9ad3c4080bdebf88597e5f1ff49f
Nl256 <73300944+Nl256@users.noreply.github.com>
2023-05-02 11:22:58 -0400

    Update modpanel.js

--- commands/modpanel.js
+++ commands/modpanel.js
@@ -1,6 +1,38 @@
 // commands/modpanel.js
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const mysql = require('mysql2/promise');
 
+// Create a MySQL connection pool
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: 'Users',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+// Function to get the config from the database
+async function getConfig() {
+  try {
+    const [rows] = await pool.query('SELECT * FROM config');
+    return rows[0];
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function query(sql, values) {
+  try {
+    const [rows] = await pool.query(sql, values);
+    return rows;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Function to send a punishment DM to a user
 async function sendPunishmentDM(user, action, reason) {
   try {
     const dmChannel = await user.createDM();
@@ -25,6 +57,10 @@ module.exports = {
     .addStringOption(option =>
       option.setName('reason')
         .setDescription('The reason for the action')
+        .setRequired(false))
+    .addIntegerOption(option =>
+      option.setName('duration')
+        .setDescription('The duration of the mute in minutes')
         .setRequired(false)),
   async execute(interaction, config) {
     // Check if the user has an allowed role
@@ -37,6 +73,7 @@ module.exports = {
     const action = interaction.options.getString('action');
     const targetUser = interaction.options.getUser('user');
     const reason = interaction.options.getString('reason') || 'No reason provided';
+    const duration = interaction.options.getInteger('duration');
 
     // Check if the target user is immune
     if (config.immuneUsers.includes(targetUser.id)) {
@@ -47,7 +84,7 @@ module.exports = {
     switch (action) {
       case 'mute':
         // Send punishment DM
-        await sendPunishmentDM(targetUser, 'muted', reason);
+        await sendPunishmentDM(targetUser, 'muted', reason, duration);
 
         // Find or create the Muted role
         let mutedRole = interaction.guild.roles.cache.find(role => role.name === 'Muted');
@@ -55,7 +92,7 @@ module.exports = {
           try {
             mutedRole = await interaction.guild.roles.create({
               name: 'Muted',
-              color: 'GREY',
+              color: '#808080',
               permissions: [],
             });
 
@@ -74,7 +111,24 @@ module.exports = {
           }
                 // Add the Muted role to the target user
     await interaction.guild.members.cache.get(targetUser.id).roles.add(mutedRole);
-    interaction.reply(`Muted ${targetUser.tag} for: ${reason}`);
+    interaction.reply(`Muted ${targetUser.tag} for: ${reason} (${duration} minutes)`);
+    // Add mute data to the database
+    await query('INSERT INTO mutes (user_id, duration) VALUES (?, ?)', [targetUser.id, duration]);
+    // Remove the Muted role from the target user after 5 minutes
+    setTimeout(async () => {
+      await targetMember.roles.remove(mutedRole);
+      interaction.followUp(`Unmuted ${targetUser.tag}.`);
+    }, duration * 60000);
+
+    // Delete mute data from the database
+    await query('DELETE FROM mutes WHERE user_id = ?', [targetUser.id]);
+    // check muted users
+    const mutedUsers = await query('SELECT * FROM mutes');
+    console.log('Muted users:', mutedUsers);
+    // remove the Muted role from the target user and delete mute data from the database
+    await interaction.guild.members.cache.get(targetUser.id).roles.remove(mutedRole);
+    await query('DELETE FROM mutes WHERE user_id = ?', [targetUser.id]);
+    interaction.reply(`Unmuted ${targetUser.tag}.`);
     break;
 
   case 'kick':
